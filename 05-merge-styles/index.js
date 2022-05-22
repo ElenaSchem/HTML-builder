2015d6fff3160a54f1fab1f9b2844ed76974c869
ElenaSchem <elena.shch91@gmail.com>
2022-05-22 13:30:07 +0300

    feat: add 05-merge-styles solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectDist = path.join(__dirname, 'project-dist');
+const styles = path.join(__dirname, 'styles');
+
+const createBundle = fs.createWriteStream(path.join(projectDist, 'bundle.css'));
+
+fs.readdir(styles, { withFileTypes: true }, (error, result) => {
+  if (error) return console.error(error.message);
+  result.forEach(file => {
+    if (path.extname(file.name) === '.css') {
+      fs.readFile(path.join(styles, file.name), (error, data) => {
+        if (error) return console.error(error.message);
+        createBundle.write(data);
+      });
+    }
+  });
+  console.log('Bundle.css created successfully');
+});
